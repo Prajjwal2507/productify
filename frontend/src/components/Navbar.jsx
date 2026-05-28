375e47ce27b3f03931f0aca8c657fc3390679cac
Prajjwal2507 <prajjwalrajput2507@gmail.com>
2026-05-29 05:03:07 +0530

    feat: integrate Clerk for authentication and user synchronization
    - Added `useAuthReq` hook to manage authentication token for API requests.
    - Implemented `useUserSync` hook to synchronize user data with the backend.
    - Updated `App` component to include authentication checks and user synchronization.
    - Enhanced `Navbar` component to utilize Clerk's authentication features.
    - Created API utility functions for user and product management.
    - Configured Axios instance for API requests with base URL and credentials.
    - Updated dependencies in `package.json` for Clerk, React Query, and Axios.
    - Refactored main entry point to include React Query provider for state management.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,29 @@
 import { Link } from "react-router";
-import { SignInButton , SignUpButton , UserButton , useAuth } from "@clerk/react";
-import { ShoppingBagIcon , PlusIcon , UserIcon } from "lucide-react" ;
+import {
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useAuth,
+} from "@clerk/react";
+import { ShoppingBagIcon, PlusIcon, UserIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
 
 function Navbar() {
-  const { isSignedIn } = useAuth() ; 
+  const { isSignedIn } = useAuth();
   return (
     <div className="navbar bg-base-300">
       <div className="max-w-5xl mx-auto w-full px-4 flex justify-between items-center">
-       {/* LOGO - LEFT SIDE */}
+        {/* LOGO - LEFT SIDE */}
         <div className="flex-1">
           <Link to="/" className="btn btn-ghost gap-2">
             <ShoppingBagIcon className="size-5 text-primary" />
-            <span className="text-lg font-bold font-mono uppercase tracking-wider">Productify</span>
+            <span className="text-lg font-bold font-mono uppercase tracking-wider">
+              Productify
+            </span>
           </Link>
         </div>
         {/* NAV LINKS - CENTER */}
-          <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center">
           <ThemeSelector />
           {isSignedIn ? (
             <>
@@ -43,7 +50,7 @@ function Navbar() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
